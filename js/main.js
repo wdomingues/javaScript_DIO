1950e5590e554acc26c71412abac30ff9264559b
Winne Domingues <wdomingues@id.uff.br>
2020-06-29 17:19:49 -0300

    1.2. Array e Dicionário

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-var nome = "WisDom TS"
-var idade1 = 2012-2005
-var idade2 = 2020-2012
-var frase = "A " + nome + " está há " + (idade1+idade2)+ " anos no mercado oferecendo o que há de melhor em tecnologia."
-alert(frase.replace("oferecendo", "fornecendo"));
+var frutas = [{nome: "jaca", cor: "verde"},
+                {nome: "cajá", cor: "amarelo"}];
+console.log(frutas);
+alert(frutas[1].nome + " " + frutas[0].cor +"?");
 
-console.log(nome);
-console.log(idade1);
-console.log(frase);
+var fruta = {nome: "maçã", cor: "vermelha"}
+console.log(fruta.nome)
+alert(fruta.cor)
+
+var lista = ["maçã", "pêra", "laranja"];
+lista.push("uva")
+lista.reverse();
+lista.pop();
+
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+
+
+//var idade1 = 2012-2005
+//var idade2 = 2020-2012
+//var frase = "A " + nome + " está há " + (idade1+idade2)+ " anos no mercado oferecendo o que há de melhor em tecnologia."
+//alert(frase.replace("oferecendo", "fornecendo"));
+
+//console.log(nome);
+//console.log(idade1);
+//console.log(frase);
