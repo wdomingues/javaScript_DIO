67b3efb4d366f38733d4287e1c6be47569da6fa4
Winne Domingues <wdomingues@id.uff.br>
2020-06-29 05:53:39 -0300

    1.1. Introdução ao JavaScript

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var nome = "WisDom TS"
+var idade1 = 2012-2005
+var idade2 = 2020-2012
+var frase = "A " + nome + " está há " + (idade1+idade2)+ " anos no mercado oferecendo o que há de melhor em tecnologia."
+alert(frase.replace("oferecendo", "fornecendo"));
+
+console.log(nome);
+console.log(idade1);
+console.log(frase);
